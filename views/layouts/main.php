78665b2557cf34414c7dadbf8306fc3ad60ac193
akenth0r <akenpendragon@gmail.com>
2021-02-25 14:42:29 +0300

    Commit

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -69,7 +69,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; Not My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy;  Double Not My Company <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
